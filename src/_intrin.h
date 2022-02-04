76b41d17ed17946b7cf37a5f01efc62e2935c85f
dsbelov63 <dsbelov63@gmail.com>
2022-02-04 16:20:46 +0400

    use cross-compiler inlining

--- src/_intrin.h
+++ src/_intrin.h
@@ -3,13 +3,21 @@
 
 
 
+#if defined(__GNUC__)
+	#define INLINE __attribute__((always_inline)) inline
+#elif defined(_MSC_VER)
+	#define INLINE __forceinline
+#endif
+
+
+
 #ifdef __wasm__
 	#include <wasm_simd128.h>
 
 	#define __m128 __f32x4
 	#define _mm_load_ps wasm_v128_load
 	#define _mm_load_ps1 wasm_v128_load32_splat
-	#define _MM_SHUFFLE(a, b, c, d) (a * 4) + 0, (a * 4) + 1, (a * 4) + 2, (a * 4) + 3, (b * 4) + 0, (b * 4) + 1, (b * 4) + 2, (b * 4) + 3, (c * 4) + 0, (c * 4) + 1, (c * 4) + 2, (c * 4) + 3, (d * 4) + 0, (d * 4) + 1, (d * 4) + 2, (d * 4) + 3
+	#define _MM_SHUFFLE(b, a, d, c) (a * 4) + 0, (a * 4) + 1, (a * 4) + 2, (a * 4) + 3, (b * 4) + 0, (b * 4) + 1, (b * 4) + 2, (b * 4) + 3, ((c + 4) * 4) + 0, ((c + 4) * 4) + 1, ((c + 4) * 4) + 2, ((c + 4) * 4) + 3, ((d + 4) * 4) + 0, ((d + 4) * 4) + 1, ((d + 4) * 4) + 2, ((d + 4) * 4) + 3
 	#define _mm_shuffle_ps __builtin_wasm_shuffle_i8x16
 	// make wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f) constant
 	#define _mm_addsub_ps(a, b) wasm_f32x4_add(a, wasm_f32x4_mul(b, wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f)))
@@ -19,7 +27,7 @@
 	#define _mm_add_ps wasm_f32x4_add
 	#define _mm_sub_ps wasm_f32x4_sub
 
-	inline __m128 _mm_loadr_ps (const float* __p)
+	INLINE __m128 _mm_loadr_ps (const float* __p)
 	{
 		__m128 __v = _mm_load_ps(__p);
 
@@ -28,6 +36,10 @@
 #else
 	#include <immintrin.h>
 #endif
+
+
+
+#undef INLINE
 
 
 
