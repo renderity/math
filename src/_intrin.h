297ef29f33532d18efa93b1ac49cd7ad194bf725
dsbelov63 <dsbelov63@gmail.com>
2022-04-29 14:37:48 +0400

    up

--- src/_intrin.h
+++ src/_intrin.h
@@ -15,10 +15,11 @@
 	#include <wasm_simd128.h>
 
 	#define __m128 __f32x4
+	#define __m128i __i32x4
 	#define _mm_load_ps wasm_v128_load
 	#define _mm_load_ps1 wasm_v128_load32_splat
-	#define _mm_addsub_ps(a, b) wasm_f32x4_add(a, wasm_f32x4_mul(b, wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f)))
 	#define _mm_store_ps wasm_v128_store
+	#define _mm_set_ps wasm_f32x4_make
 	#define _mm_set1_ps wasm_f32x4_splat
 	#define _mm_add_ps wasm_f32x4_add
 	#define _mm_sub_ps wasm_f32x4_sub
@@ -27,6 +28,7 @@
 	#define _mm_min_ps wasm_f32x4_min
 	#define _mm_max_ps wasm_f32x4_max
 	#define _mm_shuffle_ps __builtin_wasm_shuffle_i8x16
+	#define _mm_addsub_ps(a, b) wasm_f32x4_add(a, wasm_f32x4_mul(b, wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f)))
 
 	#define _MM_SHUFFLE(a, b, c, d)\
 		(d * 4), (d * 4) + 1, (d * 4) + 2, (d * 4) + 3,\
@@ -40,25 +42,25 @@
 	// 	__shuf1c, __shuf2c, __shuf3c, __shuf4c,\
 	// 	__shuf1d, __shuf2d, __shuf3d, __shuf4d,
 
-	constexpr int __shuf1 (const int x)
-	{
-		return (x + 4);
-	}
+	// constexpr int __shuf1 (const int x)
+	// {
+	// 	return (x + 4);
+	// }
 
-	constexpr int __shuf2 (const int x)
-	{
-		return ((x + 4) + 1);
-	}
+	// constexpr int __shuf2 (const int x)
+	// {
+	// 	return ((x + 4) + 1);
+	// }
 
-	constexpr int __shuf3 (const int x)
-	{
-		return ((x + 4) + 2);
-	}
+	// constexpr int __shuf3 (const int x)
+	// {
+	// 	return ((x + 4) + 2);
+	// }
 
-	constexpr int __shuf4 (const int x)
-	{
-		return ((x + 4) + 3);
-	}
+	// constexpr int __shuf4 (const int x)
+	// {
+	// 	return ((x + 4) + 3);
+	// }
 
 	/**
 	 * Used to wrap last 2 argumants of _MM_SHUFFLE
